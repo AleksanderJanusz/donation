f6fae4a82fd86728cc7b7eca4209ab02dbbd8b3f
AleksanderJanusz <oleksanderjanusz@gmail.com>
2023-06-30 13:21:42 +0200

    step 4

--- donate/forms.py
+++ donate/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from django.core.exceptions import ValidationError
-
 from donate.models import Donation
 
 
 class DonationForm(forms.ModelForm):
     quantity = forms.IntegerField(min_value=1)
+    pick_up_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    pick_up_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    pick_up_comment = forms.CharField(widget=forms.Textarea(attrs={"rows": 5}))
 
     class Meta:
         model = Donation
-        fields = ['quantity']
-
+        fields = ['quantity', 'address', 'phone_number', 'city', 'zip_code', 'pick_up_date', 'pick_up_time',
+                  'pick_up_comment']
